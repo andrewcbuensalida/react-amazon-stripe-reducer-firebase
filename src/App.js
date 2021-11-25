f01a4ea10306615fadb5d036c25a028fbbc2dc85
andrewcbuensalida <andrewcbuensalida@gmail.com>
2021-11-25 09:57:26 -0800

    front end working

--- src/App.js
+++ src/App.js
@@ -13,64 +13,64 @@ import { loadStripe } from "@stripe/stripe-js";
 import { Elements } from "@stripe/react-stripe-js";
 
 const promise = loadStripe(
-  "pk_test_51HPvU9DFg5koCdLGJJbNo60QAU99BejacsvnKvT8xnCu1wFLCuQP3WBArscK3RvSQmSIB3N0Pbsc7TtbQiJ1vaOi00X9sIbazL"
+	"pk_test_51HPvU9DFg5koCdLGJJbNo60QAU99BejacsvnKvT8xnCu1wFLCuQP3WBArscK3RvSQmSIB3N0Pbsc7TtbQiJ1vaOi00X9sIbazL"
 );
 
 function App() {
-  const [{}, dispatch] = useStateValue();
+	const [{}, dispatch] = useStateValue();
 
-  useEffect(() => {
-    // will only run once when the app component loads...
+	useEffect(() => {
+		// will only run once when the app component loads...
 
-    auth.onAuthStateChanged((authUser) => {
-      console.log("THE USER IS >>> ", authUser);
+		auth.onAuthStateChanged((authUser) => {
+			console.log("THE USER IS >>> ", authUser);
 
-      if (authUser) {
-        // the user just logged in / the user was logged in
+			if (authUser) {
+				// the user just logged in / the user was logged in
 
-        dispatch({
-          type: "SET_USER",
-          user: authUser,
-        });
-      } else {
-        // the user is logged out
-        dispatch({
-          type: "SET_USER",
-          user: null,
-        });
-      }
-    });
-  }, []);
+				dispatch({
+					type: "SET_USER",
+					user: authUser,
+				});
+			} else {
+				// the user is logged out
+				dispatch({
+					type: "SET_USER",
+					user: null,
+				});
+			}
+		});
+	}, []);
 
-  return (
-    <Router>
-      <div className="app">
-        <Switch>
-          <Route path="/orders">
-            <Header />
-            <Orders />
-          </Route>
-          <Route path="/login">
-            <Login />
-          </Route>
-          <Route path="/checkout">
-            <Header />
-            <Checkout />
-          </Route>
-          <Route path="/payment">
-            <Header />
-            <Elements stripe={promise}>
-              <Payment />
-            </Elements>
-          </Route>
-          <Route path="/">
-            <Header />
-            <Home />
-          </Route>
-        </Switch>
-      </div>
-    </Router>
-  );
+	return (
+		<Router>
+			<div className="app">
+				<Switch>
+					<Route path="/orders">
+						<Header />
+						<Orders />
+					</Route>
+					<Route path="/login">
+						<Login />
+					</Route>
+					<Route path="/checkout">
+						<Header />
+						<Checkout />
+					</Route>
+					<Route path="/payment">
+						<Header />
+						<Elements stripe={promise}>
+							<Payment />
+						</Elements>
+					</Route>
+					<Route path="/">
+						<Header />
+						<Home />
+					</Route>
+				</Switch>
+			</div>
+		</Router>
+	);
 }
 
 export default App;
--- src/firebase.js
+++ src/firebase.js
@@ -1,13 +1,13 @@
 import firebase from "firebase";
 
 const firebaseConfig = {
-  apiKey: "AIzaSyCcPSKlYtpdzBoAC8soeSmIARMzVKzrf5I",
-  authDomain: "challenge-4b2b2.firebaseapp.com",
-  databaseURL: "https://challenge-4b2b2.firebaseio.com",
-  projectId: "challenge-4b2b2",
-  storageBucket: "challenge-4b2b2.appspot.com",
-  messagingSenderId: "962418448875",
-  appId: "1:962418448875:web:f6cce5eeaf819481f661ae",
+	apiKey: "AIzaSyBW9zOT2eHzHJe6CoifW7hRIt-m9nHx8n4",
+	authDomain: "clone-b8548.firebaseapp.com",
+	projectId: "clone-b8548",
+	storageBucket: "clone-b8548.appspot.com",
+	messagingSenderId: "450950941403",
+	appId: "1:450950941403:web:e3ba9f2be2c1ccc47b1afe",
+	measurementId: "G-NL7ZZ3PJZC",
 };
 
 const firebaseApp = firebase.initializeApp(firebaseConfig);
